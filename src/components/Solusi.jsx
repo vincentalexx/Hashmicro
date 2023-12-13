789c05e63db56ad2e6e0ea01b7a57e9a83c94edd
vincent vincent <vincentalexx03@gmail.com>
2023-12-13 21:31:55 +0700

    solusi minor styling

--- src/components/Solusi.jsx
+++ src/components/Solusi.jsx
@@ -67,7 +67,7 @@ const Solusi = () => {
             </div>
             <div className="">
                 <ul className="flex bg-gray-200 rounded-3xl  font-semibold text-sm md:text-base py-1">
-                    <li className="select round-xl py-2 px-3 md:px-8 lg:px-10 cursor-pointer hover:bg-white border-2  rounded-3xl ease-out duration-300 text-gray-400 hover:text-black" id='0' onClick={() => open(0)}>
+                    <li className="select round-xl py-2 px-3 md:px-8 lg:px-10 cursor-pointer hover:bg-white border-2  rounded-3xl ease-out duration-300 text-white bg-red-700 hover:text-black" id='0' onClick={() => open(0)}>
                         <p>Manufaktur</p>
                     </li>
                     <li className="select round-xl py-2 px-3 md:px-8 lg:px-10 cursor-pointer hover:bg-white border-2  rounded-3xl ease-out duration-300 text-gray-400 hover:text-black" id='1' onClick={() => open(1)}>
